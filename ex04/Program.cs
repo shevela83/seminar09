4392bd7377c042e4e9f7815fb445bdcb6ba10ce8
Shevela_Denis <shevela83@mail.ru>
2023-03-11 08:28:37 +0300

    ex4

---
+++ ex04/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine(St (72, 3));
+double St (int a, int b){
+    if (b == 0)
+    {
+        return 1;
+    }
+    return a * St (a, b - 1);
+}
+
