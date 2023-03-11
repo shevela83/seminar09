8806c698ff08949fcdf07ab3ad7fc25a572b32dd
Shevela_Denis <shevela83@mail.ru>
2023-03-11 08:25:36 +0300

    ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,19 @@
+﻿/***Задача 67:**Напишите программу, 
+которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9*/
+
+using System;
+using static System.Console;
+
+Clear();
+WriteLine(sumnum (1234));
+int sumnum (int n){
+    if (n / 10 == 0)
+    {
+        return n;
+    }
+      
+    return n % 10 + sumnum(n/10);
+}
+
