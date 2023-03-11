53dbe3c616bd36a2c1f83141bb8ed610a9e1151f
Shevela_Denis <shevela83@mail.ru>
2023-03-11 08:24:22 +0300

    ex1

---
+++ ex01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine(PrintNumbers(10, 4));
+string PrintNumbers(int n, int m){
+    if (m > n)
+    {
+        return String.Empty;
+    }
+    string s = m.ToString() + " " + PrintNumbers(n, m +1);
+    //WriteLine(s);
+    return s;
+}
